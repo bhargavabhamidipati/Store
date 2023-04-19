4f970a157b23ad7da02fa570bd62c28d8b32369f
Bhargava Bhamidipati <84910711+bhargavabhamidipati@users.noreply.github.com>
2023-04-19 23:40:15 +0200

    Springboot-backend

---
+++ StoreBackend/src/test/java/com/example/StoreBackend/StoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
