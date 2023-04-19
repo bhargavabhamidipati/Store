14432c077c7670cc709d4dcea96fc1c5f446cab2
Bhargava Bhamidipati <84910711+bhargavabhamidipati@users.noreply.github.com>
2023-04-19 23:41:16 +0200

    application file

--- StoreBackend/src/main/java/com/example/StoreBackend/StoreBackendApplication.java
+++ StoreBackend/src/main/java/com/example/StoreBackend/StoreBackendApplication.java
@@ -2,9 +2,13 @@ package com.example.StoreBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan({"com.example.controller"})
 public class StoreBackendApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreBackendApplication.class, args);
